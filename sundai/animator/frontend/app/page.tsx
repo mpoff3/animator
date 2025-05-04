0288aa70828e525a4507cdbfb6e10005dd270b0a
Vineet Sinha <vineet.sinha@gmail.com>
2025-05-04 16:43:38 -0400

    fixing folders checked in by mistake

--- sundai/animator/frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import SearchInterface from "@/components/search-interface"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-white dark:bg-slate-950 flex flex-col items-center px-4 py-16">
-      <SearchInterface />
-    </main>
-  )
-}
